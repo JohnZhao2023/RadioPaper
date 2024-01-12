a560f75c5f47db9cfbba3769ac23da3a60546578
JohnZhao <johnzhao2023new@gmail.com>
2024-01-12 19:17:31 +0800

    private cloud change, add rouge-1 score

--- private_cloud/private_cloud.py
+++ private_cloud/private_cloud.py
@@ -1,61 +1,125 @@
-# Import required libraries
+# import the relevant libraries
 import socket
 import yaml
 from Crypto.Random import get_random_bytes
 from EncryptionDecryption import encryption_process, decrypt_ipv4_addresses
 import time
-import pandas as pd
 
-# Read configuration file
-with open('private_cloud.yaml', 'r') as file:
-    yaml_data = yaml.safe_load(file)
+# What environment variable can be set to select a different kubeconfig file when running antctl out-of-cluster in "controller mode"?
 
-# Get parameters
-public_server_addr = yaml_data['properties']['public-server-address']
-public_port = yaml_data['properties']['public-server-port']
-private_port = yaml_data['properties']['private-server-port']
 
-# Generate a random key
-key = get_random_bytes(16)
-
-# Address mapping for encryption/decryption
-address_mapping = {}
-
-# Read the CSV file containing questions
-questions_df = pd.read_csv('/path/to/your/questions/file.csv')  # Replace with the path to your questions CSV file
-
-# Private cloud server for receiving answers and ROUGE-1 scores
+# private cloud agent in charge of receiving the answer and decrypt it
 def private_cloud_server(address_mapping, host='0.0.0.0', port=8000):
+    # establish a socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # bind the host private cloud address and port 8000
         s.bind((host, port))
         s.listen()
-        print(f"Success! The private cloud {host} is listening on the port {port}...")
-
+        # show the information
+        print(f"\tSuccess! The private cloud {host} is listening on the port {port}...")
+        # receive the connection from the public server
         conn, addr = s.accept()
         with conn:
-            print('Connected by', addr)
+            # print which public server is connected
+            print('\tConnected by', addr)
+
+            # receive the data
             data = conn.recv(1024*1024*8192).decode('utf-8')
 
-            # Assuming encrypted answers and ROUGE-1 scores are in 'data'
-            decrypted_answer = decrypt_ipv4_addresses(address_mapping, data)  # Use your decryption method
-            print("\nDecrypted Answer:\n", decrypted_answer)
+            index = data.find("ROUGE-1 Score:")
+            encrypted_answer = data[:index]
+            rouge_score = float(data[index+14:])
 
-# Private cloud client for generating and sending encrypted questions
-def private_cloud_client(address_mapping, key, server_host, server_port=8000, private_port=8000):
+            # decrypt the answer using the address mapping
+            decrypted_answer = decrypt_ipv4_addresses(address_mapping, encrypted_answer)
+
+            # print
+            print("\n\tThe encrypted anwer is:\n\t", encrypted_answer)
+            print("\tThe decrypted anwer is:\n\t", decrypted_answer)
+            print("\tThe Rouge-1 Score is: ", rouge_score)
+            # print('Connection closed')
+
+
+# private cloud agent in charge of generating the question and encrypt it
+def private_cloud_client(address_mapping, key,
+                         server_host,
+                         server_port=8000,
+                         private_port=8000):
+
+    # generating the socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # connect the public cloud
         s.connect((server_host, server_port))
-        print(f"Connecting to the public cloud {server_host} succeeded!")
+        # print the connection success
+        print(f"\nconnecting to the public cloud {server_host} succeed!")
 
-        for index, row in questions_df.iterrows():
-            question = row['question']
-            encrypted_input, encrypted_markdown_content = encryption_process(address_mapping, question, "./antctl.md", key)
+        # question count
+        question_count = 1
 
+        # asking question
+        while True:
+            # get unencrypted question from the input
+            question = input(f"\nplease input No.{question_count} question, input exit to exit:\n")
+            # exit
+            if question.lower() == 'exit':
+                s.sendall('Exit'.encode('utf-8'))
+                break
+
+            # timing
+            start_time = time.time()
+
+            # encrypt the question
+            address_mapping, encrypted_input, encrypted_markdown_content = encryption_process(address_mapping, question, "./antctl.md", key)
+            # no encryption
+            # loader = TextLoader("./antctl.md")
+            # markdown_document = loader.load()
+            # encrypted_markdown_content = markdown_document[0].page_content
+            # encrypted_input = question
+
+            # data = json.dumps({
+            #     "encrypted_question": encrypted_input,
+            #     "encrypted_markdown_content": encrypted_markdown_content
+            # })
+            # sent the encrypted question and references
             data = encrypted_input + "__xxxxx__" + encrypted_markdown_content
             s.sendall(data.encode('utf-8'))
-            print(f"Question {index+1} sent to the public server.")
+            print("\n\tthe question is already sent to the public server!")
 
-            # Start listening on the port to get the decrypted answer and ROUGE-1 score
+            # start to listen to the port to get the undecrypted answer
             private_cloud_server(address_mapping, port=private_port)
 
+            # end time
+            end_time = time.time()
+
+            # print the time
+            print(f"\n\tThe time for this process is: {end_time - start_time} second")
+
+            # add the count
+            question_count = question_count + 1
+
+        # print end information
+        print('Service is closed')
+
+
+# main func
 if __name__ == "__main__":
-    private_cloud_client(address_mapping, key, server_host=public_server_addr, server_port=public_port, private_port=private_port)
+    # read yaml file
+    with open('private_cloud.yaml', 'r') as file:
+        yaml_data = yaml.safe_load(file)
+
+    # get the params
+    public_server_addr = yaml_data['properties']['public-server-address']
+    public_port = yaml_data['properties']['public-server-port']
+    private_port = yaml_data['properties']['private-server-port']
+
+    # forming the key
+    key = get_random_bytes(16)
+
+    # address mapping
+    address_mapping = {}
+
+    # private cloud
+    private_cloud_client(address_mapping, key,
+                         server_host=public_server_addr,
+                         server_port=public_port,
+                         private_port=private_port)
